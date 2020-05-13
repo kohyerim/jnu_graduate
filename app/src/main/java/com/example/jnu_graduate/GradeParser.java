c0f6b5befb92cf7b8af32822bfda5d0f7b4f5c67
Suye0n <incuriositas@naver.com>
2020-05-13 20:55:26 +0900

    read db

--- app/src/main/java/com/example/jnu_graduate/GradeParser.java
+++ app/src/main/java/com/example/jnu_graduate/GradeParser.java
@@ -1,5 +1,8 @@
 package com.example.jnu_graduate;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,9 +10,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class GradeParser {
-    public void getMajor() throws IOException {
+    public JSONObject getMajor() throws IOException, JSONException {
         try {
-            InputStream is = new URL("https://raw.githubusercontent.com/incuriositas/capstoneDatabase/master/major/test.json").openStream();
+            InputStream is = new URL("https://raw.githubusercontent.com/incuriositas/capstoneDatabase/master/gradePoint/requirements.json").openStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             String str;
             StringBuffer buffer = new StringBuffer();
@@ -17,12 +20,17 @@ public class GradeParser {
                 buffer.append(str);
             }
             String receiveMsg = buffer.toString();
-            System.out.println(receiveMsg);
+            JSONObject major =  new JSONObject(receiveMsg);
+            return major;
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return null;
     }
-    public void getCulture(int i) throws IOException {
+
+    public JSONObject getCulture(int i) throws IOException, JSONException {
         try {
             InputStream is = new URL("https://raw.githubusercontent.com/incuriositas/capstoneDatabase/master/culture/"+i+".json").openStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, "UTF-8"));
@@ -32,9 +40,11 @@ public class GradeParser {
                 buffer.append(str);
             }
             String receiveMsg = buffer.toString();
-            System.out.println(receiveMsg);
+            JSONObject culture =  new JSONObject(receiveMsg);
+            return culture;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
--- app/src/main/java/com/example/jnu_graduate/lobby.java
+++ app/src/main/java/com/example/jnu_graduate/lobby.java
@@ -22,21 +22,75 @@ public class lobby extends AppCompatActivity {
 
     Button logout;
     TextView libarts;
+    TextView cultureGP;
+    TextView majorGP;
+    TextView totalGP;
     private String jsonString;
     private JSONObject jsonObject;
+
+    GradeParser gradeParser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        cultureGP = findViewById(R.id.liberal_arts_credit);
+        majorGP = findViewById(R.id.major_credit);
+        totalGP = findViewById(R.id.whole_credit);
+
         //왜인진 모르겟지만 이안에 뭔가 코드를 작성하려고 들면 액티비티가 실행이 안됨...
+        // 제 생각에는 아마 manifests에 laylout 추가 안해서 그런것 같아요!!
+
+        new Thread(){
+            public void run(){
+                gradeParser = new GradeParser();
+                JSONObject gradeInfo = null;
+                try {
+                    // 학점 db 불러오기
+                    gradeInfo = gradeParser.getMajor();
+//                    System.out.println(gradeInfo);
+
+                    String myHakbeon = "2017";
+                    String majorClass = "컴퓨터공학전공";
+                    String currentGrade = "3학년";
+                    String currentSemester = "2학기";
+
+                    // 교양학점 db조회
+                    JSONObject gradePoint = (JSONObject) gradeInfo.get("교양학점");
+                    JSONObject cultureGradePoint = (JSONObject) gradePoint.get(myHakbeon);
+                    Object minGrade = cultureGradePoint.get("min");
+                    Object maxGrade = cultureGradePoint.get("max");
+                    cultureGP.setText(minGrade.toString()+'-'+maxGrade.toString());
+
+                    // 졸업학점 db 조회
+                    JSONObject majorInfo = (JSONObject) gradeInfo.get(majorClass);
+                    JSONObject majorGradePoint = (JSONObject) majorInfo.get(myHakbeon);
+                    Object jolupGP = majorGradePoint.get("졸업학점");
+                    Object major = majorGradePoint.get("심화전공");
+                    totalGP.setText(jolupGP.toString());
+                    majorGP.setText(major.toString());
+
+                    // 학번별 전공필수과목 db조회
+                    JSONObject majorSub = (JSONObject) majorGradePoint.get("전공필수");
+                    JSONObject grade = (JSONObject) majorSub.get(currentGrade);
+                    Object subject = grade.get(currentSemester);
+//                    System.out.println(subject);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
         gologout();
         golibarts_page();
         try {
             setProfile();
         } catch (JSONException e) {
             e.printStackTrace();
-        }
+        };
 
     }
     public void gologout(){
@@ -89,7 +143,6 @@ public class lobby extends AppCompatActivity {
         } catch (IOException e) {
             Log.e("login activity", "Can not read file: " + e.toString());
         }
-
 
         jsonObject = new JSONObject(jsonString);
 
